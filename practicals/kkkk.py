43244d3ab2225a31198a6f58e32030c1158454a2
Mohammed Alzimam <mohammedalzimam@149-161-190-83.dhcp-bl.indiana.edu>
2020-10-15 10:54:41 -0400

    My commit message

---
+++ practicals/kkkk.py
@@ -0,0 +1,20 @@
+import sys
+
+b = sys.stdin.read()
+
+k=b.replace('.' , ' .')
+k=k.replace(',' , ' , ')
+k=k.replace('(' , ' (')
+k=k.replace(')' , ' )')
+k=k.replace('/', '/  ')
+k=k.replace('\\', '\ ')
+k=k.replace(':' , ' :')
+k=k.replace('and' , ' and')
+for sentencs in k.split('\n'):
+        print(sentencs)
+        for token in sentencs.split(' '):
+                print(token, '          -       -       -       -       -      $
+
+
+
+
